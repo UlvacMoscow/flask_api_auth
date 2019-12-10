318d33dd73d9c42ce3d1e880b80b3338091019dd
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2019-12-10 22:11:12 +0300

    add model user

---
+++ project/models.py
@@ -0,0 +1,8 @@
+from flask_secutiry import UserMixin
+
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(255))
