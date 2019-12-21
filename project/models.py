4e64225a5cf046842d33281a846dc74e36076a6f
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2019-12-21 14:29:37 +0300

    update users

--- project/models.py
+++ project/models.py
@@ -4,5 +4,7 @@ from flask_secutiry import UserMixin
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(100))
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(255))
+    age = db.Column(db.Integer())
